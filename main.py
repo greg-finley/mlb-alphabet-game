da1b83ced86ee2691cb46d537d7f907a35bc95a6
Greg Finley <gregory.finley@gmail.com>
2022-09-19 19:39:28 -0700

    Skip completed games

--- main.py
+++ main.py
@@ -45,15 +45,11 @@ class Play:
     batter_name: str
     batter_id: int
 
-    @property
-    def endTime_pacific(self) -> str:
-        """Change a UTC time like "2022-09-18T01:06:53.861Z" to Pacific time"""
-        return (
-            datetime.datetime.strptime(self.endTime, "%Y-%m-%dT%H:%M:%S.%fZ")
-            .replace(tzinfo=utc)
-            .astimezone(timezone("US/Pacific"))
-            .strftime("%-m/%-d %-I:%M %p Pacific")
-        )
+
+@dataclass
+class Game:
+    game_id: int
+    is_complete: bool
 
 
 def main(event, context):
@@ -61,37 +57,48 @@ def main(event, context):
     bigquery_client = BigQueryClient()
     twitter_client = TwitterClient()
 
+    # Get games we have already completely process so we don't poll them again
+    completed_games = bigquery_client.get_recently_completed_games()
+
+    # Poll for today's games and find all the plays we haven't processed yet
+    games = mlb_client.get_current_games(completed_games)
+
+    if not games:
+        print("No incomplete games")
+        return
+
     # Get the previous state from BigQuery
     state = bigquery_client.get_initial_state()
     print(state)
+    unprocessed_plays = mlb_client.get_unprocessed_plays(games, state)
 
-    # Poll for today's games and find all the plays we haven't processed yet
-    game_ids = mlb_client.get_current_game_ids()
-    unprocessed_plays = mlb_client.get_unprocessed_plays(game_ids, state)
-
-    if unprocessed_plays:
-        for p in unprocessed_plays:
-            if p.is_hit and state.current_letter in p.batter_name.upper():
-                # Find all matches in the batter's name amid the upcoming letters and update the state
-                matching_letters: list[str] = []
-                while state.current_letter in p.batter_name.upper():
-                    matching_letters.append(state.current_letter)
-                    state.current_letter = state.next_letter
-                    if state.current_letter == "A":
-                        state.times_cycled += 1
-
-                # Tweet it
-                twitter_client.tweet(p, state, matching_letters)
-
-        # At the end, update BigQuery with any state changes and the last end time
-        state.last_time = unprocessed_plays[-1].endTime
-        bigquery_client.update_state(state)
-    else:
+    if not unprocessed_plays:
         print("No new plays")
+        return
+
+    for p in unprocessed_plays:
+        if p.is_hit and state.current_letter in p.batter_name.upper():
+            # Find all matches in the batter's name amid the upcoming letters and update the state
+            matching_letters: list[str] = []
+            while state.current_letter in p.batter_name.upper():
+                matching_letters.append(state.current_letter)
+                state.current_letter = state.next_letter
+                if state.current_letter == "A":
+                    state.times_cycled += 1
+
+            # Tweet it
+            twitter_client.tweet(p, state, matching_letters)
+
+    # At the end, update BigQuery with any state changes and the last end time
+    state.last_time = unprocessed_plays[-1].endTime
+    bigquery_client.update_state(state)
+    for g in games:
+        if g.is_complete:
+            bigquery_client.set_completed_game(g)
 
 
 class MLBClient:
-    def get_current_game_ids(self) -> list[int]:
+    def get_current_games(self, completed_games: list[int]) -> list[Game]:
         # Fudge it by a day in either direction in case of timezone issues
         today = datetime.date.today()
         yesterday = (today - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
@@ -102,20 +109,28 @@ class MLBClient:
             + f"/schedule?sportId=1&startDate={yesterday}&endDate={tomorrow}"
         ).json()["dates"]
 
-        game_ids: list[int] = []
+        games: list[Game] = []
         for d in dates:
             for g in d["games"]:
-                game_ids.append(g["gamePk"])
-        return game_ids
+                game_id = g["gamePk"]
+                abstract_game_state = g["status"]["abstractGameState"]
+                if abstract_game_state != "Preview" and game_id not in completed_games:
+                    games.append(
+                        Game(
+                            game_id=game_id,
+                            is_complete=abstract_game_state == "Final",
+                        )
+                    )
+        return games
 
-    def get_unprocessed_plays(self, game_ids: list[int], state: State) -> list[Play]:
+    def get_unprocessed_plays(self, games: list[Game], state: State) -> list[Play]:
         """Get the plays that we haven't processed yet and sort them by endTime."""
         plays: list[Play] = []
 
-        for g in game_ids:
-            all_plays = requests.get(MLB_API_BASE_URL + f"/game/{g}/playByPlay").json()[
-                "allPlays"
-            ]
+        for g in games:
+            all_plays = requests.get(
+                MLB_API_BASE_URL + f"/game/{g.game_id}/playByPlay"
+            ).json()["allPlays"]
             for p in all_plays:
                 if p["about"]["isComplete"] and (
                     DRY_RUN or p["about"]["endTime"] > state.last_time
@@ -175,9 +190,9 @@ class TwitterClient:
             alert = f"""🚨 {alert_name} LETTER 🚨
 
 """
-        tweet_text = f"""{alert}{play.batter_name} just hit a {play.event.lower()} at {play.endTime_pacific}!
+        tweet_text = f"""{alert}{play.batter_name} just hit a {play.event.lower()}!
 
-His name has the letter{'' if len(matching_letters) == 1 else 's'} {oxford_comma(matching_letters)}, so the next letter in the MLB Alphabet Game is now {state.current_letter}!
+His name has the letter{'' if len(matching_letters) == 1 else 's'} {oxford_comma(matching_letters)}, so the next letter in the MLB Alphabet Game is now {state.current_letter}.
 
 We have cycled through the alphabet {state.times_cycled} times since this bot was created on 9/17."""
         print(tweet_text)
@@ -202,6 +217,28 @@ We have cycled through the alphabet {state.times_cycled} times since this bot wa
 class BigQueryClient:
     def __init__(self) -> None:
         self.client = bigquery.Client()
+
+    def get_recently_completed_games(self) -> list[int]:
+        if DRY_RUN:
+            return []
+        query = """
+            SELECT game_id
+            FROM mlb_alphabet_game.completed_games
+            WHERE completed_at > TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 4 DAY)
+        """
+        query_job = self.client.query(query)
+        results = query_job.result()
+        game_ids = [r.game_id for r in results]
+        return game_ids
+
+    def set_completed_game(self, game: Game) -> None:
+        q = f"""
+            INSERT INTO mlb_alphabet_game.completed_games (game_id, completed_at)
+            VALUES ({game.game_id}, CURRENT_TIMESTAMP())
+        """
+        print(q)
+        if not DRY_RUN:
+            self.client.query(q).result()
 
     def get_initial_state(self) -> State:
         rows = self.client.query(
