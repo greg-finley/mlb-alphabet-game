524f93821990fed1c3066feca77d5a2fde3771aa
Greg Finley <gregory.finley@gmail.com>
2022-09-22 09:03:59 -0700

    Make BigQuery use sport column

--- clients/bigquery_client.py
+++ clients/bigquery_client.py
@@ -15,6 +15,7 @@ class BigQueryClient:
         query = """
             SELECT game_id
             FROM mlb_alphabet_game.completed_games
+            where sport = 'MLB'
             order by completed_at desc limit 100
         """
         query_job = self.client.query(query)
@@ -24,8 +25,8 @@ class BigQueryClient:
 
     def set_completed_game(self, game: Game) -> None:
         q = f"""
-            INSERT INTO mlb_alphabet_game.completed_games (game_id, completed_at)
-            VALUES ({game.game_id}, CURRENT_TIMESTAMP())
+            INSERT INTO mlb_alphabet_game.completed_games (game_id, sport, completed_at)
+            VALUES ({game.game_id}, 'MLB', CURRENT_TIMESTAMP())
         """
         print(q)
         if not self.dry_run:
@@ -33,7 +34,7 @@ class BigQueryClient:
 
     def get_initial_state(self) -> State:
         rows = self.client.query(
-            "SELECT current_letter, times_cycled, last_time FROM mlb_alphabet_game.state;"
+            "SELECT current_letter, times_cycled, last_time FROM mlb_alphabet_game.state where sport = 'MLB';"
         )
         # Will only have one row
         for row in rows:
@@ -41,7 +42,7 @@ class BigQueryClient:
         raise Exception("No state found")
 
     def update_state(self, state: State) -> None:
-        q = f"UPDATE mlb_alphabet_game.state SET current_letter = '{state.current_letter}', times_cycled = {state.times_cycled}, last_time = '{state.last_time}' WHERE 1=1;"
+        q = f"UPDATE mlb_alphabet_game.state SET current_letter = '{state.current_letter}', times_cycled = {state.times_cycled}, last_time = '{state.last_time}' WHERE sport='MLB';"
         print(q)
         if not self.dry_run:
             self.client.query(q).result()
