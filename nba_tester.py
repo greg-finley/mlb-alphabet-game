5c2504563cbb6bd8901e23f382b3c731a82f5b4c
Greg Finley <gregory.finley@gmail.com>
2022-09-30 22:14:47 -0700

    More NBA research

--- nba_tester.py
+++ nba_tester.py
@@ -1,6 +1,16 @@
+import requests
+
 from clients.nba_client import NBAClient
 
 nba = NBAClient(dry_run=True)
 
 # print(nba.get_current_games([]))
-print(nba._get_player_name(1626149))
+# print(nba._get_player_name(1626149))
+
+all_plays = requests.get(
+    "https://cdn.nba.com/static/json/liveData/playbyplay/playbyplay_0012200002.json"
+).json()["game"]["actions"]
+for p in all_plays:
+    play_id = str(p["actionNumber"])
+    if p.get("shotResult") == "Made" and p.get("subType") == "DUNK":
+        print(p["period"], p["clock"], p["description"])
