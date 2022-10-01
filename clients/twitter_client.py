f1ff64ee493b39c9905c21c4ef4679bd9342dc73
Greg Finley <gregory.finley@gmail.com>
2022-09-30 21:28:23 -0700

    Add more options

--- clients/twitter_client.py
+++ clients/twitter_client.py
@@ -8,9 +8,9 @@ from my_types import ImageInput, State, TweetablePlay
 from clients.abstract_sports_client import AbstractSportsClient
 from clients.image_client import ImageClient
 
-SAD_EMOJIS = ["😭", "😢", "❌", "😔"]
+SAD_EMOJIS = ["😭", "😢", "❌", "😔", "😮‍💨"]
 
-SAD_PHRASES = ["Darn", "Drats", "Oh shoot", "Oh no", "Bad luck"]
+SAD_PHRASES = ["Darn", "Drats", "Oh shoot", "Oh no", "Bad luck", "Bummer", "Sigh"]
 
 
 class TwitterClient:
